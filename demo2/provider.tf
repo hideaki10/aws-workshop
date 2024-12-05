ca864a9b23d491eb508cac45f5aa4056bc566fdc
hideaki10 <senshikou1985@gmail.com>
2024-12-05 23:28:20 +0900

    edit comment

--- demo2/provider.tf
+++ demo2/provider.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "ap-northeast-1"  # 东京区域
+  region = "ap-northeast-1"  # 
 }
 
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"  # 指定AWS provider的版本
+      version = "~> 4.0"  # 
     }
   }
 } 
